21de46ff938a901369d57292efce28c7ee04c269
Gregory Alexandre <121134598+GregoryAlexandre369@users.noreply.github.com>
2024-04-20 19:02:07 -0400

    Add initialCards array to index.js

---
+++ scripts.js/index.js
@@ -0,0 +1,27 @@
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/sof/Yosemite_Valley.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/sof/Lake_Louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/sof/Bald_Mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/sof/Latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/sof/Vanoise_National_Park.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/sof/Lago_di_Braies.jpg",
+  },
+  // You can continue adding more objects as needed
+];
